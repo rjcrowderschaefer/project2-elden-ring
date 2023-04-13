cf4ee3d02645d92b1f88236facfcd355eb765060
RJ Crowder-Schaefer <rj.crowder@gmail.com>
2023-04-12 21:15:03 -0400

    set up classes index and show pages

--- controllers/characterInformation.js
+++ controllers/characterInformation.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const CharacterInformation = require('../models/character-info/CharacterInfo');
 const characterInfo = require('../seed-data/character-info-seed/CharacterInfo-seed');
+const Classes = require('../models/character-info/classes');
+const classesInfo = require('../seed-data/character-info-seed/Classes-seed');
 
 router.get('', async (req, res, next) => {
     try {
@@ -15,6 +17,28 @@ router.get('', async (req, res, next) => {
     }
 })
 
-// not doing additional routes for this index page and given that we will hard code the four categories on the index.ejs page for Character Info
+router.get('/Classes', async (req, res, next) => {
+    try {
+        await Classes.deleteMany({});
+        const newClassesInfo = await Classes.insertMany(classesInfo);
+        res.render('classes/index.ejs', {classesInfo: newClassesInfo});
+    } catch(err) {
+        console.log(err);
+        next();
+    }
+})
+
+router.get('/Classes/:name', async (req, res, next) => {
+    try {
+        const myClassesInfo = await Classes.findOne({name: req.params.name});
+        console.log(myClassesInfo);
+        res.render('classes/show', {myClassesInfo})
+    } catch(err) {
+        console.log(err);
+        next();
+    }
+})
+
+
 
 module.exports = router;
