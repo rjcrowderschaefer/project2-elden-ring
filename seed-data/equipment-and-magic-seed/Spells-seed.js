44eec03c72fc742dcc7c8251e91ef5aeae0c663d
kobyjackcheese <kwrape@icloud.com>
2023-04-15 10:14:12 -0500

    re edited spells, lost progress

--- seed-data/equipment-and-magic-seed/Spells-seed.js
+++ seed-data/equipment-and-magic-seed/Spells-seed.js
@@ -1,21 +1,45 @@
 const Spells = [
     {
-        name: '',
-        img: '',
-        description: '',
-        officalKobyRating: '',
-        effect: '',
-        type: '',
-        class: '',
-        requiredMemory: '',
-        requirements: {
-            str: '',
-            dex: '',
-            int: '',
-            fai: '',
-            arc: ''
-        }
+        sorceries: [
+            {
+                name: '',
+                img: '',
+                description: '',
+                officalKobyRating: '',
+                effect: '',
+                type: '',
+                class: '',
+                requiredMemory: '',
+                requirements: {
+                    str: '',
+                    dex: '',
+                    int: '',
+                    fai: '',
+                    arc: ''
+                }
+            }
+        ],
+        incantations: [
+            {
+                name: '',
+                img: '',
+                description: '',
+                officalKobyRating: '',
+                effect: '',
+                type: '',
+                class: '',
+                requiredMemory: '',
+                requirements: {
+                    str: '',
+                    dex: '',
+                    int: '',
+                    fai: '',
+                    arc: ''
+                }
+            }
+        ]
     }
+
 ]
 
 module.exports = Spells
