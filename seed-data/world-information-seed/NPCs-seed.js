b6a04b3afe23c595bb30ed22ca83d953f8816166
kobyjackcheese <kwrape@icloud.com>
2023-04-16 10:40:48 -0500

    put an item in each catagory in seedtata

--- seed-data/world-information-seed/NPCs-seed.js
+++ seed-data/world-information-seed/NPCs-seed.js
@@ -1,4 +1,10 @@
 const NPCs = [
-
+    {
+        name: 'Ranni The Witch',
+        img: 'https://eldenring.wiki.fextralife.com/file/Elden-Ring/ranni-the-witch-npc-elden-ring-wiki-guide-300px.jpg',
+        description: 'Ranni the Witch is an NPC in Elden Ring. Ranni the Witch is a mysterious figure who first introduces herself as Renna. Upon meeting her again at Three Sisters, she reveals her true name and eventually will recruit you into her servitude, sending you off in search of a hidden treasure. This page covers vital information for Rannis questline for Elden Ring.',
+        location: '	Rannis Rise, Three Sisters',
+        role: 'Main NPC - Affects Endings',
+    }
 ]
 module.exports = NPCs;
