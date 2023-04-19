5e9bc4f350fa6b7cc2a008018f858c80e840d59f
RJ Crowder-Schaefer <rj.crowder@gmail.com>
2023-04-19 03:19:58 +0100

    Updated equipment magic seed data

--- seed-data/equipment-and-magic-seed/Equipment-and-magic-seed.js
+++ seed-data/equipment-and-magic-seed/Equipment-and-magic-seed.js
@@ -1,19 +1,23 @@
 const EquipmentAndMagic =  [
     {
         name: 'Damage Types',
-        img: 'https://eldenring.wiki.fextralife.com/file/Elden-Ring/ash_of_war_giant_hunt_elden_ring_wiki_guide_200px.png'
+        img: 'https://eldenring.wiki.fextralife.com/file/Elden-Ring/ash_of_war_giant_hunt_elden_ring_wiki_guide_200px.png',
+        urlName: 'Damage_Types',
     },
     {
         name: 'Talismans',
-        img: 'https://eldenring.wiki.fextralife.com/file/Elden-Ring/crimson_seed_talisman_talisman_elden_ring_wiki_guide_200px.png'
+        img: 'https://eldenring.wiki.fextralife.com/file/Elden-Ring/crimson_seed_talisman_talisman_elden_ring_wiki_guide_200px.png',
+        urlName: 'Talismans',
     },
     {
         name: 'Spells',
-        img: 'https://eldenring.wiki.fextralife.com/file/Elden-Ring/terra_magicus_sorcery_elden_ring_wiki_guide_200px.png'
+        img: 'https://eldenring.wiki.fextralife.com/file/Elden-Ring/terra_magicus_sorcery_elden_ring_wiki_guide_200px.png',
+        urlName: 'Spells',
     },
     {
         name: 'Weapons',
-        img: 'https://eldenring.wiki.fextralife.com/file/Elden-Ring/claymore_weapon_elden_ring_wiki_guide_200px.png'
+        img: 'https://eldenring.wiki.fextralife.com/file/Elden-Ring/claymore_weapon_elden_ring_wiki_guide_200px.png',
+        urlName: 'Weapons',
     },
 ]
 module.exports = EquipmentAndMagic;
