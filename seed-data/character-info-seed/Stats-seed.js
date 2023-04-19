1542bd55d867c948c37ef57c5d60b81fb8be33e0
RJ Crowder-Schaefer <rj.crowder@gmail.com>
2023-04-19 03:13:54 +0100

    Updated routes

--- seed-data/character-info-seed/Stats-seed.js
+++ seed-data/character-info-seed/Stats-seed.js
@@ -15,7 +15,7 @@ const Stats = [
         name: 'Endurnace',
         softCap: '50',
         img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6mptdO-wO51Rezmg0R436Bu8L66ulgXwzag&usqp=CAU',
-        discription: 'Attribute governing Stamina. Also affects Robustness. Determines how heavy your equipment can be, a.k.a. Equip Load.'
+        description: 'Attribute governing Stamina. Also affects Robustness. Determines how heavy your equipment can be, a.k.a. Equip Load.'
     },
     {
         name: 'Strength',
--- seed-data/equipment-and-magic-seed/Equipment-and-magic-seed.js
+++ seed-data/equipment-and-magic-seed/Equipment-and-magic-seed.js
@@ -1,26 +1,4 @@
-const equipmentAndMagicInfo =  [
-    {
-        name: 'Damage Types',
-        urlName: 'Damage_Types',
-        img: 'https://eldenring.wiki.fextralife.com/file/Elden-Ring/ash_of_war_giant_hunt_elden_ring_wiki_guide_200px.png'
-    },
-    {
-        name: 'Talismans',
-        urlName: 'Talismans',
-        img: 'https://eldenring.wiki.fextralife.com/file/Elden-Ring/crimson_seed_talisman_talisman_elden_ring_wiki_guide_200px.png'
-    },
-    {
-        name: 'Spells',
-        urlName: 'Spells',
-        img: 'https://eldenring.wiki.fextralife.com/file/Elden-Ring/terra_magicus_sorcery_elden_ring_wiki_guide_200px.png'
-    },
-    {
-        name: 'Weapons',
-        urlName: 'Weapons',
-        img: 'https://eldenring.wiki.fextralife.com/file/Elden-Ring/claymore_weapon_elden_ring_wiki_guide_200px.png'
-    },
-]
-module.exports = equipmentAndMagicInfo;const EquipmentAndMagic =  [
+const EquipmentAndMagic =  [
     {
         name: 'Damage Types',
         img: 'https://eldenring.wiki.fextralife.com/file/Elden-Ring/ash_of_war_giant_hunt_elden_ring_wiki_guide_200px.png'
